2aecacc892a0de5ac84fbfcd18be8c9d132041d1
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-12-13 22:33:43 -0800

    built in

---
+++ inventory-app/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import {Text, Flex, Buttons} from '@chakra-ui/react'
+
+export default function Home() {
+
+
+  return (
+    <>
+    
+    <Flex w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)"> 
+      
+    </Flex>
+    </>
+  
+
+  )
+}
